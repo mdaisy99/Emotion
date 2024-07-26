5a24c52d88e46f2a318db150507a76e5ba3c4fd0
Minsy09 <minsyeon0909@gmail.com>
2024-07-26 10:39:37 +0900

    AlarmHelper 파일 추가

--- app/src/main/java/com/example/myapplication/Meditation.kt
+++ app/src/main/java/com/example/myapplication/Meditation.kt
@@ -14,31 +14,7 @@ import android.content.BroadcastReceiver
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import android.content.Context
 import android.widget.TextView
-import java.util.Calendar
 
-object AlarmHelper {
-
-    fun setMidnightAlarm(context: Context) {
-        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        val intent = Intent(context, MidnightReceiver::class.java)
-        val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-
-        // 자정 시간 설정
-        val calendar = Calendar.getInstance().apply {
-            timeInMillis = System.currentTimeMillis()
-            set(Calendar.HOUR_OF_DAY, 0)
-            set(Calendar.MINUTE, 0)
-            set(Calendar.SECOND, 0)
-            set(Calendar.MILLISECOND, 0)
-        }
-
-        // 다음 자정으로 설정하기 위해 24시간을 더함
-        val triggerAtMillis = calendar.timeInMillis + AlarmManager.INTERVAL_DAY
-
-        // 반복 알람 설정
-        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, triggerAtMillis, AlarmManager.INTERVAL_DAY, pendingIntent)
-    }
-}
 
 class Meditation : AppCompatActivity() {
 
